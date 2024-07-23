0b879826c2eca42cc526fddc5a7a2a97273d0c71
judycoding <157567395+judycoding@users.noreply.github.com>
2024-07-23 10:57:55 +0330

    Add files via upload

--- AV/script.js
+++ AV/script.js
@@ -27,7 +27,7 @@ class UI{
         row.innerHTML = `
             <td>${book.title}<td/>
             <td>${book.author}<td/>
-            <td>${book.isbn}<td/>
+            
             <td><a href="#" class="btn btn-danger btn-sm delete">X</a><td/>
         
             `;
