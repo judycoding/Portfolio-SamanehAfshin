205972d39b034e2d194ed82a98e93314b0fddaf7
judycoding <157567395+judycoding@users.noreply.github.com>
2025-07-17 13:00:03 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+const myproject = document.querySelector(".mybutton3");
+const containerProject = document.querySelector(".right-div")
+
+myproject.addEventListener("click", ()=>{
+  containerProject.style.display="block";
+})
